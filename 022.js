416ad60677e674105a93b1150cd66a99eb6a330b
Nadia <nadiadunaeva@gmail.com>
2022-11-18 16:16:47 +0100

    "8 kyu"

---
+++ 022.js
@@ -0,0 +1,5 @@
+let ArrowFunc = function (arr) {
+  return arr.map((a) => String.fromCharCode(a)).join(''); //Complete this function
+};
+
+// const ArrowFunc = (arr) => String.fromCharCode(...arr);
