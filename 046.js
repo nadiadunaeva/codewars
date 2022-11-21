ae6cb6b8fe14ff787c6fe877a3d3eca1fe41b928
Nadia <nadiadunaeva@gmail.com>
2022-11-21 23:23:52 +0100

    "8 kyu"

---
+++ 046.js
@@ -0,0 +1,9 @@
+class Ball {
+  constructor(ballType = 'regular') {
+    this.ballType = ballType;
+  }
+}
+
+// var Ball = function(ballType) {
+//     this.ballType = ballType || 'regular';
+//   };
